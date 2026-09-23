dddb8be6145ca546fce5a5339d8d8a755c704669
Martin Blicha <martin.blicha@gmail.com>
2026-09-23 13:09:36 +0200

    Debug

--- scripts/ci/pack_soljson.sh
+++ scripts/ci/pack_soljson.sh
@@ -9,6 +9,8 @@ output="$3"
 
 (( $# == 3 )) || { >&2 echo "Usage: $0 soljson.js soljson.wasm packed_soljson.js"; exit 1; }
 
+cat "${soljson_js}"
+
 # If this changes in an emscripten update, it's probably nothing to worry about,
 # but we should double-check when it happens and adjust the tail command below.
 [[ $(head -c 11 "${soljson_js}") == "use strict;" ]] || { >&2 echo 'Expected soljson.js to start with "use strict;"'; exit 1; }
